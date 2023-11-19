0189aa19169ef8e654188325339e8aa03e0b79b0
Simone Mandola <s.mandola.design@hotmail.com>
2023-11-19 18:46:42 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,10 @@ import { store } from './store.jsx'
 import { PrimeReactProvider } from 'primereact/api';
 import App from './App.tsx'
 
+// theme
+import "primereact/resources/themes/soho-light/theme.css";
+import "primeicons/primeicons.css";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <Provider store={store}>
