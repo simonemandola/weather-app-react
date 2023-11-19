47eef69f701916b314608b5696ddb07fc02ddf6c
Simone Mandola <s.mandola.design@hotmail.com>
2023-11-19 15:46:30 +0100

    update icon url

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
     return (
         <article className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-[#FFE9AD] to-[#f8c07f] z-50">
             <h1 className="flex items-center flex-col justify-start gap-y-2 pb-32">
-                <img src="src/assets/icon.svg" width="60" height="60" alt="weather app icon" />
+                <img src="/icon.svg" width="60" height="60" alt="weather app icon" />
                 <span>
                     <i className="pi pi-spinner animate-spin mr-2" />
                     Cargando...
